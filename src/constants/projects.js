608bd30c284a13f4f9ff1af501e782e5b0c3a50f
Anten Manuuel <anten.manuuel@stonybrook.edu>
2024-01-15 13:55:37 -0500

    add a project

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,5 +1,5 @@
 import { stackoverflow, games } from "../assets/icons";
-
+import  { c } from "../assets/icons";
 export const projects = [
     {
         iconUrl: games,
@@ -14,5 +14,13 @@ export const projects = [
         name: 'Fake Stack Overflow Clone',
         description: 'Built with React, Express.js, Material-UI, and MongoDB, mirrors 15 key features of Stack Overflow, focusing on enhanced security and improved user experience. It features robust user authentication using bcrypt and a 50% faster page load through efficient pagination, blending a secure, responsive backend with a sleek, intuitive frontend.',
         link: 'https://github.com/antenmanuuel/FakeStackOverFlowClone',
+    },
+
+    {
+        iconUrl: c,
+        theme: 'btn-back-white',
+        name: 'WolfPack Packet Handler',
+        description: 'Handle and manage data using the WolfPack Protocol. This project offers simple tools to break down, check, and rebuild data packets just like IPv4. Perfect for understanding basic network tasks. Dive in and start managing packets with ease!',
+        link: 'https://github.com/antenmanuuel/WolfPackPacketHandler',
     }
 ];
